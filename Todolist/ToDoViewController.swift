b8b963202115e477922d9435d9b6c4c00c199270
Danila Bolshakov <danila1999777@gmail.com>
2023-09-13 11:25:17 +0300

    Initial Commit

---
+++ Todolist/ToDoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoViewController.swift
+//  Todolist
+//
+//  Created by iOS - Developer on 13.09.2023.
+//
+
+import UIKit
+
+class ToDoViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
